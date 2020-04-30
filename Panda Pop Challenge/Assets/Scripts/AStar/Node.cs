146a1628cfb56ed246cb8a2225f2db43b6b9c982
Mauro Sanchez <maurosanchez.work@gmail.com>
2020-04-30 15:50:45 -0300

    Project Upload

---
+++ Panda Pop Challenge/Assets/Scripts/AStar/Node.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace PathFinding
+{
+    public class Node : IAStarNode
+    {
+        public Vector3 nodePosition = Vector3.zero;
+        List<IAStarNode> neighbourList = new List<IAStarNode>();
+        public NodeType nodeType = NodeType.Grass;
+        public GameObject tileObject = null;
+
+        // Day cost of the different types. Defined as const so they are not modified.
+        // Water is always a maximum value, so the algorithm never take it in count.
+        const int GRASS_DAYS = 1;
+        const int FOREST_DAYS = 3;
+        const int DESERT_DAYS = 5;
+        const int MOUNTAIN_DAYS = 10;
+        const int WATER_DAYS = int.MaxValue;
+
+        // GValue contain the value of moving to this tile. Defined by DefineNodeType() on constructor.
+        public int gValue = 0;
+
+        public enum NodeType
+        {
+            //Contains the type of nodes that can exist
+            Grass = 0, Forest= 1, Desert = 2, Mountain = 3, Water = 4
+        }
+
+        public Node(Vector3 position)
+        {
+            //Node constructor.
+            this.nodePosition = position;
+            DefineNodeType();
+        }
+
+        public void DefineNodeType(bool random = true)
+        {
+            //If Random is TRUE, define a random tile. If it's FALSE, it change to the next tile type. It's used so the user can change the tile behavior with right click.
+
+            if (random)
+            {
+                nodeType = (NodeType)UnityEngine.Random.Range(0, 5);
+            } else
+            {
+                nodeType++;
+                if((int)nodeType > 4)
+                {
+                    nodeType = 0;
+                }
+            }
+
+            //Change the GCost
+            if(nodeType == NodeType.Grass)
+            {
+                gValue = GRASS_DAYS;
+            } else if(nodeType == NodeType.Forest)
+            {
+                gValue = FOREST_DAYS;
+            } else if(nodeType == NodeType.Desert)
+            {
+                gValue = DESERT_DAYS;
+            } else if(nodeType == NodeType.Mountain)
+            {
+                gValue = MOUNTAIN_DAYS;
+            } else if(nodeType == NodeType.Water)
+            {
+                gValue = WATER_DAYS;
+            }
+        }
+
+        public void AddNeighbour(Node nbr)
+        {
+            //This function allows the NodeCreator to reference the neighbours of this node.
+            if (!neighbourList.Contains(nbr))
+            {
+                neighbourList.Add(nbr);
+            }
+        }
+
+        IEnumerable<IAStarNode> IAStarNode.Neighbours
+        {
+            get
+            {
+                //Return the neighbour nodes.
+                return neighbourList;
+            }
+        }
+
+
+        float IAStarNode.CostTo(IAStarNode next)
+        {
+            //Search if the next node is a neighbour. If it is, return the day cost of moving to that tile.
+            foreach(Node node in neighbourList)
+            {
+                if(node == next)
+                {
+                    return node.gValue;
+                } else
+                {
+                    continue;
+                }
+            }
+            //Else return max value so the algorithm don't take in count before others.
+            return int.MaxValue;
+
+        }
+        float IAStarNode.EstimatedCostTo(IAStarNode target)
+        {
+            //Find the target node position calling the main Node List on Node Creator.
+            Node lastNode = NodeCreator.instance.FindNode(target);
+            Vector3 lastPos = lastNode.nodePosition;
+
+            //Check the distance between this node and the last node.
+            var distance = Vector3.Distance(lastNode.nodePosition, lastPos);
+
+            //Multiply by the biggest cost suposing all tiles are the same (Not water)
+            return distance * MOUNTAIN_DAYS;
+
+        }
+
+    }
+}
---
+++ Panda Pop Challenge/Assets/Scripts/NodeCreator.cs
@@ -0,0 +1,435 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PathFinding
+{
+    public class NodeCreator : MonoBehaviour
+    {
+        //For Singleton
+        public static NodeCreator instance { get; private set; }
+
+        [Header("Tile Prefab")]
+        [Tooltip("The Grid is constructed with this GameObject.")]
+        [SerializeField] GameObject nodeObj = null;
+
+        [Space]
+        [Tooltip("Different textures of each tipe of tile")]
+        [Header("Tile Textures")]
+        [SerializeField] Texture t_grass = null;
+        [SerializeField] Texture t_forest, t_desert, t_mountain, t_water = null;
+
+        [Space]
+        [Tooltip("The grid will be constructed between the margins of this Vector. Can be changed from inspector and pre visualized with a Gizmo.")]
+        [Header("Map Size")]
+        [SerializeField] Vector3 mapSize = Vector3.one;
+
+        [Space]
+        [Tooltip("Allow the user to change the colors of the tiles when a path is choosen.")]
+        [Header("Path Colors")]
+        [SerializeField] Color startNodeColor = Color.green;
+        [SerializeField] Color currentPathColor = Color.red;
+        [SerializeField] Color oldPathColor = Color.blue;
+        [SerializeField] Color waterColor = Color.blue;
+
+        //Contain a List of all existing nodes on screen.
+        List<Node> nodeList = new List<Node>();
+
+        //Final path reference.
+        IList<IAStarNode> finalPath = new List<IAStarNode>();
+
+        //Define the start and end node when searching a path.
+        Node startNode = null;
+        Node endNode = null;
+
+        //Just for tile positioning when actives.
+        float inactiveZ = 0;
+        float activeZ = 0;
+
+        private void Awake()
+        {
+            //The Singleton check if there's no other Node Creator so there won't be another object creating other node layer.
+            if (instance == null)
+            {
+                instance = this;
+            } else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Create the nodes on screen.
+            CreateNodes();
+        }
+
+        private void Update()
+        {
+            //Check the mouse activity.
+            GetMouseClick();
+        }
+
+
+        #region Node Creation (Main Functions)
+        private void CreateNodes()
+        {
+            // This function creates the points in the map where the tiles will be placed.
+
+            if (nodeObj == null)
+            {
+                // In case the tile Prefab is not selected, it returns an error.
+                Debug.LogWarning("There's no node object selected!");
+                return;
+            }
+
+
+            //The map Limit is selected with a Vector3. The tiles won't go forward the Vector3 margins.
+            Vector3 mapLimit = mapSize / 2;
+
+            //The starting point of the first tile is in the center of the GameObject who possess this component. Create the first node and add it to a list of references.
+            Vector3 startPos = transform.position;
+            Node startNode = new Node(startPos);
+            nodeList.Add(startNode);
+
+            //For this case, it puts a limit of 500 tiles if the margins aren't reached before.
+            for (var i = 0; i < 500; i++)
+            {
+                if (i >= nodeList.Count)
+                {
+                    //Break when all nodes are between the margins.
+                    break;
+
+                }
+                Node currentNode = nodeList[i];
+
+                //Get a list with the neighbour positions
+                foreach (Vector3 neighbour in NeighbourPositions(currentNode.nodePosition, nodeObj.transform.localScale))
+                {
+                    //Check if a Tile GameObject exists in the neighbour position. 
+                    //Since the positions are float points (not exacts), uses Mathf.Approximately to check it properly.
+                    Node neighbourNode = nodeList.Find(n => Mathf.Approximately(n.nodePosition.x, neighbour.x) && Mathf.Approximately(n.nodePosition.y, neighbour.y));
+
+                    //If a neighbour exists, add it to the current node neighbours, else, it creates the node and adds it.
+                    if (neighbourNode != null)
+                    {
+                        currentNode.AddNeighbour(neighbourNode);
+                    }
+                    else
+                    {
+                        neighbourNode = new Node(neighbour);
+                        currentNode.AddNeighbour(neighbourNode);
+                        nodeList.Add(neighbourNode);
+                    }
+                }
+
+            }
+
+            //Now all the nodes had been created in the positions. Now creates the objects.
+            CreateMap();
+        }
+        private void CreateMap()
+        {
+            //This function creates the GameObjects on the map once all the Positions are setted and make a reference in each node to the correspondent GameObject (Tile).
+
+            foreach(Node node in nodeList)
+            {
+                var newTile = Instantiate(nodeObj, node.nodePosition, Quaternion.identity);
+                var renderer = newTile.GetComponentInChildren<Renderer>();
+
+
+                node.tileObject = newTile;
+                renderer.material.mainTexture = GetNodeTexture(node);
+            }
+
+            //Just set active and inactive Z position.
+            inactiveZ = nodeList[0].tileObject.transform.position.z;
+            activeZ += 0.1f;
+        }
+        private Texture GetNodeTexture(Node node)
+        {
+            //Based on the node type, it add the correct texture to the tile.
+            if (node.nodeType == Node.NodeType.Grass)
+            {
+                return t_grass;
+            }
+            else if (node.nodeType == Node.NodeType.Forest)
+            {
+                return t_forest;
+            }
+            else if (node.nodeType == Node.NodeType.Desert)
+            {
+                return t_desert;
+            }
+            else if (node.nodeType == Node.NodeType.Mountain)
+            {
+                return t_mountain;
+            }
+            else if (node.nodeType == Node.NodeType.Water)
+            {
+                return t_water;
+            } else
+            {
+                return null;
+            }
+        }
+
+        private List<Vector3> NeighbourPositions(Vector3 center, Vector3 size)
+        {
+            //This list returns the neighbour position in the six directions of the hexagon. 
+            //It's based on the center of the node and the neighbours are multiplied by the node object size. This way the positions are always correct even if the object changes the size.
+
+            float centerScaleX = size.x;
+            float centerScaleY = size.y;
+            Vector3 centerPosition = center;
+            List<Vector3> positionList = new List<Vector3>();
+            List<Vector3> checkedList = new List<Vector3>();
+            Vector3 currentPos = new Vector3();
+
+            Vector3 mapLimit = mapSize / 2;
+
+            bool checkMapLimit = currentPos.x > (-mapLimit.x) && currentPos.x < mapLimit.x && currentPos.y < mapLimit.y && currentPos.y > (-mapLimit.y);
+
+            // Left side
+            currentPos = centerPosition + (Vector3.left * centerScaleX);
+            positionList.Add(currentPos);
+
+            currentPos = centerPosition + (Vector3.left * centerScaleX / 2) + (Vector3.up * centerScaleY / 1.35f);
+            positionList.Add(currentPos);
+
+            currentPos = centerPosition + (Vector3.left * centerScaleX / 2) + (Vector3.down * centerScaleY / 1.35f);
+            positionList.Add(currentPos);
+
+            // Right side
+            currentPos = centerPosition + (Vector3.right * centerScaleX);
+            positionList.Add(currentPos);
+
+            currentPos = centerPosition + (Vector3.right * centerScaleX / 2) + (Vector3.up * centerScaleY / 1.35f);
+            positionList.Add(currentPos);
+
+            currentPos = centerPosition + (Vector3.right * centerScaleX / 2) + (Vector3.down * centerScaleY / 1.35f);
+            positionList.Add(currentPos);
+
+
+            //Once we have the six neighbour positions, we check if it's in the margins of the map size (Inside our Vector3 named mapSize)
+            foreach(Vector3 pos in positionList)
+            {
+                checkMapLimit = pos.x > (-mapLimit.x) && pos.x < mapLimit.x && pos.y < mapLimit.y && pos.y > (-mapLimit.y);
+
+                if (checkMapLimit)
+                {
+                    checkedList.Add(pos);
+                }
+            }
+
+            //Return the Vectors of the neighbours that can exist in the map size.
+            return checkedList;
+        }
+
+
+        #endregion
+
+        #region Mouse Function
+        private void GetMouseClick()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                //Get a Ray on the mouse position. (Left click)
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if(Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    //Send the clicked object to the pathfinder.
+                    SetPath(hit.transform);
+                }
+            }
+
+            if(Input.GetMouseButtonDown(1))
+            {
+                //Get a Ray on the mouse position. (Right click)
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    //Change this node behavior.
+                    StartCoroutine(ChangeNodeBehavior(hit.transform));
+                }
+            }
+        }
+
+        #endregion
+
+        #region Path Functions
+        private void SetPath(Transform node)
+        {
+            //This function set the Start and End point depending in the current activity.
+            //This is the latest pressed node.
+            Node currentNode = nodeList.Find(n => n.tileObject == node.transform.gameObject);
+
+            //If the user is pressing water, make a short animation and return.
+            if(currentNode.nodeType == Node.NodeType.Water)
+            {
+                Debug.Log("You are clicking on water!");
+                StartCoroutine(ClickOnWater(currentNode));
+                return;
+            }
+
+
+            if (startNode == null)
+            {
+                //If there's no start point, set the node as the first point.
+                startNode = currentNode;
+            }
+            else
+            {
+                //If there's a start point, set the current node as the end of the path.
+                endNode = currentNode;
+
+                //If the start and the end point are the same, reset the nodes.
+                if (startNode == endNode)
+                {
+                    ResetAllNodes(true);
+                    return;
+                }
+
+                // This function change the color of the previous path under the new one. User can change the color of the old path on inspector to watch already traversed nodes. 
+                ColorPreviousPath(); 
+
+                //If everything is okay, Show the path between the Start and End points.
+                finalPath = AStar.GetPath(startNode, endNode);
+                ShowPathOnNodes(finalPath);
+
+                //Once the path is done, make the End the new Start, so the player can continue searching from the last point.
+                startNode = endNode;
+            }
+
+            //Change color to the start and end points.
+            node.transform.position += new Vector3(0, 0, -activeZ);
+            node.transform.GetComponentInChildren<Renderer>().material.color = startNodeColor;
+        }
+        private void ResetAllNodes(bool resetStartAndEnd = false)
+        {
+            //This clean the start and end node references with the old paths.
+            if(resetStartAndEnd)
+            {
+                startNode = null;
+                endNode = null;
+                finalPath.Clear();
+            }
+
+            //This function return all nodes to the original inactive state.
+            foreach(Node node in nodeList)
+            {
+                if(node == startNode) { continue; }
+
+                Transform nodeObj = node.tileObject.transform;
+                nodeObj.position = new Vector3(nodeObj.position.x, nodeObj.position.y, inactiveZ);
+                nodeObj.GetComponentInChildren<Renderer>().material.color = Color.white;
+            }
+        }
+
+        #endregion
+
+        #region Display and Responsive Functions
+        private void ShowPathOnNodes(IList<IAStarNode> list)
+        {
+            //This function take the list returned from GetPath() method and paint the nodes on screen.
+            List<Node> finalPath = new List<Node>();
+            foreach(IAStarNode node in list)
+            {
+                finalPath.Add(nodeList.Find(n => n == node));
+            }
+
+            //Paint and move up the path nodes.
+            foreach (Node node in finalPath)
+            {
+                if (node == startNode) { continue; }
+                    GameObject thisNode = node.tileObject;
+                    var theRenderer = thisNode.GetComponentInChildren<Renderer>().material.color = currentPathColor;
+                    thisNode.transform.position += new Vector3(0, 0, -activeZ);
+
+            }
+
+            // -------- DEBUG ONLY! This function draws a Debug Line between the path nodes.
+            //var lineI = 0;
+            //foreach(Node node in finalPath)
+            //{
+            //    try
+            //    {
+            //        Debug.DrawLine(finalPath[lineI].nodePosition, finalPath[lineI + 1].nodePosition, Color.red, 5f);
+            //        lineI++;
+            //    }
+            //    catch
+            //    {
+            //        return;
+            //    }
+            //}
+        }
+        private void ColorPreviousPath()
+        {
+            //This function color the old paths under the new ones.
+            //The color can be changed on inspector. It's white by default, so it won't show if there's no change from inspector.
+
+            //If there's no previous path, just return.
+            if(finalPath.Count == 0 || finalPath == null) { return; }
+
+            //Change the color of the previous path.
+            foreach(IAStarNode node in finalPath)
+            {
+                if(node == startNode || node == endNode) { continue; }
+
+                var thisNode = nodeList.Find(n => n == node).tileObject;
+                thisNode.GetComponentInChildren<Renderer>().material.color = oldPathColor;
+                thisNode.transform.position += new Vector3(0, 0, inactiveZ);
+            }
+        }
+        IEnumerator ClickOnWater(Node waterNode)
+        {
+            //This function reject the user when pressing on water tiles and make a tiny response.
+            var waterObj = waterNode.tileObject;
+            var waterRenderer = waterObj.GetComponentInChildren<Renderer>();
+
+            waterObj.transform.position += new Vector3(0, 0, activeZ);
+            waterRenderer.material.color = waterColor;
+            yield return new WaitForSeconds(0.2f);
+            waterObj.transform.position -= new Vector3(0, 0, activeZ);
+            waterRenderer.material.color = Color.white;
+        }
+
+        #endregion
+
+        #region Extra Functions
+        IEnumerator ChangeNodeBehavior(Transform Node)
+        {
+            //This function allows the User to switch a node behavior and value on runtime.
+            //It's a Coroutine so the function can be responsive for the user.
+
+            Node nodeToChange = nodeList.Find(n => n.tileObject == Node.gameObject);
+            var nodeObj = nodeToChange.tileObject;
+            nodeToChange.DefineNodeType(false);
+            nodeObj.GetComponentInChildren<Renderer>().material.mainTexture = GetNodeTexture(nodeToChange);
+
+
+            nodeObj.transform.position += new Vector3(0, 0, activeZ);
+            yield return null;
+            nodeObj.transform.position -= new Vector3(0, 0, activeZ);
+
+        }
+
+        public Node FindNode(IAStarNode node)
+        {
+            //Allows to find a specific node object.
+            return nodeList.Find(n => n == node);
+        }
+        #endregion
+
+
+        private void OnDrawGizmos()
+        {
+            //This gizmo is in charge of showing on screen the size of the Map. The map tiles will be drawn between the mapSize (Vector3) margin.
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(transform.position, mapSize);
+        }
+    }
+}
